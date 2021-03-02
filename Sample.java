06076950fc6893b3cd63d940ca972804006da2af
Ravindra Desai <pravm@allstate.com>
2021-03-02 13:48:36 +0530

    UPDATED

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ class public Hello
 {
     public static void main(String[] args)
     {
-        for(int i=0;i<=15;i++)
+        for(int i=0;i<=20;i++)
         {
             System.out.print("HELLO-Everyone");
             System.out.println("Wel-Come to ALLSTATE");
